e00fee1b7ac8140fe494603487ea03d0f57bb12e
Kshitij Chowdhary <138766939+KshitijChowdhary@users.noreply.github.com>
2023-11-15 13:57:27 +0000

    Quiz Game

---
+++ quiz_brain.py
@@ -0,0 +1,34 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        current_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: \n")
+        self.check_answer(current_question.answer, current_answer)
+
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list):
+            return self.question_number < len(self.question_list)
+        else:
+            print("You've completed the quiz!")
+            print(f"Your final score is: {self.score}/{self.question_number}.")
+            return self.question_number < len(self.question_list)
+
+    def check_answer(self, correct_answer, current_answer):
+        if current_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+
+        else:
+            print("You got it wrong!")
+
+        print(f"The correct answer was {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
+
+
+
